983e90c8e9533095da4e5813b1fb2008c8de5916
Caerols <brian.caerols@gmail.com>
2025-04-24 03:10:16 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "nutriasana-v1";
+const urlsToCache = ["index.html", "styles.css", "script.js", "img/dumbbel-svgrepo-com.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
